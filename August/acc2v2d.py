2dce0ecd6bec7acf26ff29014c5855087a96dadd
SEU0710 <seu.ztx@88.com>
2024-08-20 13:51:48 +0800

    两车间距

--- August/acc2v2d.py
+++ August/acc2v2d.py
@@ -2,28 +2,48 @@
 Autor: Zhang Tianxiang
 Date: 2024-08-20 12:44:01
 LastEditors: Zhang Tianxiang
-LastEditTime: 2024-08-20 13:04:32
+LastEditTime: 2024-08-20 13:40:34
 '''
 import numpy as np
 import matplotlib.pyplot as plt
 
-dot_num = 100  # 一百次采样
-action_num = 10
+dot_num = 1000  # 一百次采样
+action_num = 100
 acc = np.random.choice((-2, -1, 0, 1, 2), action_num)  # 10次加减速动作
-t = np.arange(0, 100, 10)
-tt = np.arange(0, 100, 1)
+acc2 = np.random.choice((-2, -1, 0, 1, 2), action_num)  # 10次加减速动作
+print(acc)
+print(acc2)
+t = np.linspace(0, 100, action_num)
+tt = np.linspace(0, 100, dot_num)
 acc_i = acc.repeat(int(dot_num/action_num))  # 
+acc2_i = acc2.repeat(int(dot_num/action_num))  # 
 v = []
+v2 = []
+v_init = 40
 for i in range(dot_num):
-    v.append(np.trapz(acc_i[:i+1], tt[:i+1]))
+    v.append(np.trapz(acc_i[:i+1], tt[:i+1])+40)
+    v2.append(np.trapz(acc2_i[:i+1], tt[:i+1])+40)
 d = []
+d2 = []
+dd = []
+d_init = 10
 for i in range(dot_num):
     d.append(np.trapz(v[:i+1], tt[:i+1]))
+    d2.append(np.trapz(v2[:i+1], tt[:i+1]))
+    dd.append(np.trapz(v[:i+1], tt[:i+1]) - np.trapz(v2[:i+1], tt[:i+1]) + d_init)
 fig, ax = plt.subplots()
 ax.plot(tt, acc_i)
 ax.plot(tt, v)
-ax.plot(tt, d)
-ax.legend(['a','v','c'])
+ax.legend(['a','v'])
+fig, ax2 = plt.subplots()
+ax2.plot(tt, acc2_i)
+ax2.plot(tt, v2)
+ax2.legend(['a2','v2'])
+fig, ax3 = plt.subplots()
+# ax3.plot(tt, d)
+# ax3.plot(tt, d2)
+ax3.plot(tt, np.abs(dd))
+# ax3.legend(['d','d2', 'dd'])
 plt.show()
 
 
